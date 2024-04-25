b7e56f03c3174b2d3c353eb069a6179cb7d403c7
Walber Costa <walberoc@gmail.com>
2024-04-25 13:26:48 -0300

    anatomia de classe 3

--- 01-SINTAXE-JAVA/java-anatomia-classes/src/MinhaClasse.java
+++ 01-SINTAXE-JAVA/java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,26 @@
 public class MinhaClasse {
     public static void main(String[] args) {
         System.out.println("Olá Java");
+    
+        // anatomia de classes 3
+            //variáveis
+        String menNome =  "Walber";
+        int anoFabricacao = 2022;
+        boolean verdadeiro = true;
+        anoFabricacao = 2018;
+
+        String primeiroNome = "Walber";
+        String segundoNome = "Costa";
+
+                // variável     parâmetro
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+    // métodos
+    public static String formatarCep(String cep){
+        return cep;
+    }
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "resultado: "+primeiroNome.concat(" ").concat(segundoNome);
     }
 }
