e32171c499e1ee3235327ca70163310067fca94d
Walber Costa <walberoc@gmail.com>
2024-04-29 17:12:14 -0300

    alterado o valor da 'não constante'

--- 02-TIPOS-E-VARIAVIES/tipos-e-variaveis/src/edu/walber/semana2/TiposEvariaveis.java
+++ 02-TIPOS-E-VARIAVIES/tipos-e-variaveis/src/edu/walber/semana2/TiposEvariaveis.java
@@ -3,7 +3,7 @@ package edu.walber.semana2;
 public class TiposEvariaveis {
     public static void main(String[] args) {
         int numero = 5;
-
+        numero = 10;
         System.out.println(numero);
     }
 }
