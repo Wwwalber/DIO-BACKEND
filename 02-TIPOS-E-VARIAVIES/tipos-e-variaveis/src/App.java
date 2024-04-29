d2b5d9e390f9a32c905302cfca850a9754ceeb4a
Walber Costa <walberoc@gmail.com>
2024-04-29 17:08:04 -0300

    update - tipos e variáveis 3

--- 02-TIPOS-E-VARIAVIES/tipos-e-variaveis/src/App.java
+++ 02-TIPOS-E-VARIAVIES/tipos-e-variaveis/src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        double salarioMinimo = 2500; // dois mil e quinhentos
+        double salarioMinimo2 = 2.500; // dois e meio (2,5)
+        double salarioMinimo3 = 2500.80; // dois mil quinhentos e oitenta
+        double salarioMinimo4 = 2,500.80; // dois mil quinhentos e oitenta
+
+        short numeroCurto = 1;
+        int numeroDecimal = numeroCurto; // ok, short com certeza caberá em um int
+        short numeroCurto2 = numeroDecimal; /*  apesar de sabermos que nesse caso
+                                na nossa aplicação mas caso em algum momento venha
+                                um valor muito grande com certeza daria erro. Por
+                                isso o Java nem permite esse atribuição. A não
+                                ser que se faça um cast, como abaixo (é uma
+                                adaptação, estando consciente de que não é
+                                consistente já que pode ocorre um erro)
+                                */
+        short numeroCurto3 = (short) numeroDecimal;                                
     }
 }
