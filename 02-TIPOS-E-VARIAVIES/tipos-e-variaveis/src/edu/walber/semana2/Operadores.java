e1f9c55ad269c66883939085e2b3060ae33468aa
Walber Costa <walberoc@gmail.com>
2024-04-30 11:53:24 -0300

    concatenando strings

---
+++ 02-TIPOS-E-VARIAVIES/tipos-e-variaveis/src/edu/walber/semana2/Operadores.java
@@ -0,0 +1,8 @@
+package edu.walber.semana2;
+
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM "+"JAVA";
+        System.out.println(nomeCompleto);
+    }
+}
