7d5cc1c96a751a8229cd050dad3f04767c1a5ede
Walber Costa <walberoc@gmail.com>
2024-05-02 04:41:53 -0300

    testes de concatenação ok

--- 02-TIPOS-E-VARIAVIES/tipos-e-variaveis/src/edu/walber/semana2/Operadores.java
+++ 02-TIPOS-E-VARIAVIES/tipos-e-variaveis/src/edu/walber/semana2/Operadores.java
@@ -2,7 +2,20 @@ package edu.walber.semana2;
 
 public class Operadores {
     public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM "+"JAVA";
-        System.out.println(nomeCompleto);
+        // classe Operadores.java
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        // qual o resultado das expressoes abaixo?
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+        concatenacao = "1" + (1 + 1 + 1);
     }
 }
