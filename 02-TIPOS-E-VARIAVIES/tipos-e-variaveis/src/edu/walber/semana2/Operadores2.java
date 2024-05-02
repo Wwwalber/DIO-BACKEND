435f8f07f71f93df495878da4d387a5ca7715023
Walber Costa <walberoc@gmail.com>
2024-05-02 04:56:18 -0300

    operador não alterou a variável principal

---
+++ 02-TIPOS-E-VARIAVIES/tipos-e-variaveis/src/edu/walber/semana2/Operadores2.java
@@ -0,0 +1,10 @@
+package edu.walber.semana2;
+
+public class Operadores2 {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        System.out.println(- numero);
+        System.out.println(numero);
+    }
+}
