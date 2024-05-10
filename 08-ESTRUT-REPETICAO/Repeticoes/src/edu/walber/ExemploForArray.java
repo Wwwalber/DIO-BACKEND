959cf87a46ff84af5fe60913de865bb0f32b2a86
Walber Costa <walberoc@gmail.com>
2024-05-10 20:59:51 -0300

    exemplos de repetição for

---
+++ 08-ESTRUT-REPETICAO/Repeticoes/src/edu/walber/ExemploForArray.java
@@ -0,0 +1,11 @@
+package edu.walber;
+
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE","WALBER","JONAS","ELIZANGELA","ISRAELE"};
+
+        for(int x=0;x<alunos.length;x++){
+            System.out.println("O aluno no índice x="+x+" é "+alunos[x]);
+        }
+    }
+}
