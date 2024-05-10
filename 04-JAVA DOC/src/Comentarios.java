dd48dd4c74f925b2f3c008f4c319143f4cd2a48d
Walber Costa <walberoc@gmail.com>
2024-05-10 04:17:37 -0300

    update ignore de Java doc

---
+++ 04-JAVA DOC/src/Comentarios.java
@@ -0,0 +1,32 @@
+public class Comentarios {
+    // comentário de uma única linha
+
+    /*
+     * comentário de várias linhas
+     */
+
+     /**
+      * comentário para documentação
+      */
+    public void metodo(){
+
+    }
+
+    /*
+    * Este método foi elaborado as pressas
+    * por isso eu abrevei o nome das variáveis
+    * mas olha, ele tem a finalidade de somar ou  multiplicar
+    * dois números
+    */
+    public int somaMultiplica(int n, int x, String m){
+        int r = 0; // r é igual ao resultado
+        if (m == "M"){ // M= multiplicação
+            r= n * x;
+        }else{
+            // se não soma mesmo
+            r = n + x;
+        }
+        return r;
+    }
+
+}
