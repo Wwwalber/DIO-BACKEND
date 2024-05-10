a96a8ce1c61eef5598f7d0232b31f85ac4c55951
Walber Costa <walberoc@gmail.com>
2024-05-10 17:53:37 -0300

    condicional ternária (composta)

--- 07-CONDICIONAIS/src/edu/walber/ResultadoEscolar.java
+++ 07-CONDICIONAIS/src/edu/walber/ResultadoEscolar.java
@@ -2,8 +2,8 @@ package edu.walber;
 
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 4;
-        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        int nota = 9;
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota <7 ? "Recuperação" : "Reprovado";
 
         System.out.println(resultado);
     }
