7857da78979f85f73617c0ab3b13c78ad72bb21f
Walber Costa <walberoc@gmail.com>
2024-05-17 16:48:58 -0300

    tratar excessão das inserções

--- 10-DEBUGGING/app-debbuging/src/edu/walber/debbuging/CalculadoraDeMedias.java
+++ 10-DEBUGGING/app-debbuging/src/edu/walber/debbuging/CalculadoraDeMedias.java
@@ -1,18 +1,22 @@
 package edu.walber.debbuging;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalculadoraDeMedias {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String[] alunos = {"Camila","Lucas","Bruna","Pedro"};
+        String[] alunos = { "Camila", "Lucas", "Bruna", "Pedro" };
+        try {
+            double media = calculaMediaDaTurma(alunos, scan);
 
-        double media = calculaMediaDaTurma(alunos, scan);
-
-        System.out.printf("Média da turma %.1f", media);
+            System.out.printf("Média da turma %.1f", media);
+        } catch (InputMismatchException e) {
+            System.out.println("Você deve fornecer um número!");
+        }
     }
 
-    public static double calculaMediaDaTurma(String[] alunos, Scanner scanner){
+    public static double calculaMediaDaTurma(String[] alunos, Scanner scanner) {
         double soma = 0;
         for (String aluno : alunos) {
             System.out.printf("Nota do aluno %s: ", aluno);
