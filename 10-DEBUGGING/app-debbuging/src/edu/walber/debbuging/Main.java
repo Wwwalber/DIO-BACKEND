1c920450db8758837f2f02b14761b420ec51acad
Walber Costa <walberoc@gmail.com>
2024-05-17 16:03:13 -0300

    commit inicial do projeto de debbuging

---
+++ 10-DEBUGGING/app-debbuging/src/edu/walber/debbuging/Main.java
@@ -0,0 +1,33 @@
+package edu.walber.debbuging;
+
+public class Main {
+    // em Java sempre começa e termina no main
+    public static void main(String[] args) {
+        System.out.println("Iniciou do programa no método main.");
+        a();
+        System.out.println("Finalizou do programa no método main.");
+    }
+
+    // omitido por default o public
+    static void a() {
+        System.out.println("Entrou no método a.");
+        b();
+        System.out.println("Finalizou o método a.");
+    }
+
+    static void b() {
+        System.out.println("Entrou no método b.");
+        for(int i = 0; i <= 4; i++) System.out.println(i);
+        c();
+        System.out.println("Finalizou o método b.");
+    }
+
+    static void c(){
+        System.out.println("Entrou no método c.");
+
+        // chamado para depuração
+        Thread.dumpStack();
+        System.out.println("Finalizou o método c.");
+    }
+
+}
