86ab1f583698b7731872916931f7207159d47736
Walber Costa <walberoc@gmail.com>
2024-05-18 10:26:05 -0300

    selecionando os candidatos

--- 11-VALIDACAO-PSELETIVO/controle-candidatos/src/edu/walber/candidatura/ProcessoSeletivo.java
+++ 11-VALIDACAO-PSELETIVO/controle-candidatos/src/edu/walber/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,41 @@
 package edu.walber.candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("Processo seletivo:");
-
+/* 
         analisarCandidato(1900.0);
         analisarCandidato(2200.0);
         analisarCandidato(2000.0);
+        */
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos(){
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            // busca mais candidatos
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato "+candidato+" solicitou este valor de salário "+salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato "+candidato+" foi selecionado para a vaga");
+                candidatosSelecionados++;// selecionado um candidato
+            }
+            candidatoAtual++;// verificar outro candidato
+        }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
@@ -18,4 +47,5 @@ public class ProcessoSeletivo {
             System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS");
         }
     }
+
 }
