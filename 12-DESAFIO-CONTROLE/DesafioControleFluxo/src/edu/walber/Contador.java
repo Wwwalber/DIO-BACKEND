67ac339a1d95ce957124298c7ee52012357c80a0
Walber Costa <walberoc@gmail.com>
2024-05-18 13:12:28 -0300

    desafio do contador ok

--- 12-DESAFIO-CONTROLE/DesafioControleFluxo/src/edu/walber/Contador.java
+++ 12-DESAFIO-CONTROLE/DesafioControleFluxo/src/edu/walber/Contador.java
@@ -13,7 +13,8 @@ public class Contador {
     
         try {
             // chamar método com lógica de contagem
-            contar(parametroUm,parametroDois);
+            int iteracoes = contar(parametroUm,parametroDois);
+            imprimir(iteracoes);
         } catch (ParametrosInvalidosException e) {
             //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
             System.out.println("O segundo parâmetro deve ser maior que o primeiro");
@@ -21,10 +22,16 @@ public class Contador {
         }
     }
 
-    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+    static int contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
         if (parametroUm > parametroDois)
             throw new ParametrosInvalidosException();
         int contagem = parametroDois - parametroUm;
+        return contagem;
     }
     
+    static void imprimir(int iteracoes){
+        for (int index = 0; index < iteracoes; index++) {
+            System.out.println("Imprimindo o número "+(index+1));
+        }
+    }
 }
