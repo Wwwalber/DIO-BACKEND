764a2ba24f600214db1342d94f66b1ce6cc9ede8
Walber Costa <walberoc@gmail.com>
2024-05-22 04:38:00 -0300

    desafio controle simples ok

---
+++ 13-LOOP-EXCECOES/SistemaBancario/src/edu/walber/ControleSimplesDeSaques.java
@@ -0,0 +1,32 @@
+package edu.walber;
+
+import java.util.Scanner;
+
+public class ControleSimplesDeSaques {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe o limite diário");
+        double limiteDiario = scanner.nextDouble();
+
+        // Loop for para iterar sobre os saques
+        for (int i = 1; i<limiteDiario; i++) {
+            System.out.println("Informe o "+i+"º saque:");
+            double valorSaque = scanner.nextDouble();
+
+            if (valorSaque == 0) {
+                System.out.println("Transacoes encerradas.");
+                break; // Encerra o loop
+            } else if (valorSaque > limiteDiario) {
+                System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+                break; // Encerra o loop
+            } else {
+                limiteDiario = limiteDiario - valorSaque;
+                System.out.println("Saque realizado. Limite restante: "+limiteDiario);
+            }
+        }
+
+        // Fechar o scanner para evitar vazamentos de recursos
+        scanner.close();
+    }
+}
