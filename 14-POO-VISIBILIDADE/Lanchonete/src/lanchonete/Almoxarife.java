ee9b50fdb1adaa13928015d283d66ac8c00c030b
Walber Costa <walberoc@gmail.com>
2024-05-23 19:45:22 -0300

    commit inicial lanchonete

---
+++ 14-POO-VISIBILIDADE/Lanchonete/src/lanchonete/Almoxarife.java
@@ -0,0 +1,22 @@
+package lanchonete;
+
+public class Almoxarife {
+
+    public void controlarEntrada() {
+		System.out.println("CONTROLANDO A ENTRADA DOS ITENS");
+	}
+	public void controlarSaida() {
+		System.out.println("CONTROLANDO A SAIDA DOS ITENS");
+	}
+
+    public void trocarGas() {
+        System.out.println("ENTREGANDO INGREDIENTES");
+        throw new UnsupportedOperationException("Unimplemented method 'trocarGas'");
+    }
+
+    public void entregarIngredientes() {
+		System.out.println("ALMOXARIFE TROCANDO O GÁS");
+        throw new UnsupportedOperationException("Unimplemented method 'entregarIngredientes'");
+    }
+
+}
