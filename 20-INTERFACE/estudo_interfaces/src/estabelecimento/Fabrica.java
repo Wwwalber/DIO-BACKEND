c833bb7f048b60b8523e64d2798cc0e3e8f29401
Walber Costa <walberoc@gmail.com>
2024-05-26 12:21:16 -0300

    comprovando uso de interfaces

--- 20-INTERFACE/estudo_interfaces/src/estabelecimento/Fabrica.java
+++ 20-INTERFACE/estudo_interfaces/src/estabelecimento/Fabrica.java
@@ -1,13 +1,20 @@
 package estabelecimento;
 
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
 import equipamentos.impressora.Deskjet;
 import equipamentos.impressora.Impressora;
 import equipamentos.impressora.LaseJet;
 import equipamentos.multifuncional.EquipamentoMultifuncional;
 
 public class Fabrica {
-    public static void main(String[] args) {
-        Impressora impressora = new EquipamentoMultifuncional();
+    public static void main(String[] args) {        
+        Impressora impressora = new LaseJet();
         impressora.imprimir();
+
+        EquipamentoMultifuncional emf = new EquipamentoMultifuncional();
+        Copiadora copiadora = emf; copiadora.copiar();
+        Digitalizadora digitalizadora = emf; digitalizadora.digitalizar();
+        Impressora impressora2 = emf; impressora2.imprimir();
     }
 }
