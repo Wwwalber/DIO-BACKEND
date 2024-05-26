3252fbeda3397a492acb12bf48b11af4c1b27ffb
Walber Costa <walberoc@gmail.com>
2024-05-26 11:50:20 -0300

    testando o uso de interface

--- 20-INTERFACE/estudo_interfaces/src/estabelecimento/Fabrica.java
+++ 20-INTERFACE/estudo_interfaces/src/estabelecimento/Fabrica.java
@@ -1,7 +1,13 @@
 package estabelecimento;
 
+import equipamentos.impressora.Deskjet;
+import equipamentos.impressora.Impressora;
+import equipamentos.impressora.LaseJet;
+import equipamentos.multifuncional.EquipamentoMultifuncional;
+
 public class Fabrica {
     public static void main(String[] args) {
-        
+        Impressora impressora = new EquipamentoMultifuncional();
+        impressora.imprimir();
     }
 }
