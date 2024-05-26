a22969ac85e97afee578818912bd97e5e92c6e2f
Walber Costa <walberoc@gmail.com>
2024-05-26 07:24:09 -0300

    acrescentando métodos reutilizáveis junto com os contratos

--- 19-PILARES-POO/PilaresDaPOO/src/mensageiros/ServicoMensagemInstantanea.java
+++ 19-PILARES-POO/PilaresDaPOO/src/mensageiros/ServicoMensagemInstantanea.java
@@ -5,4 +5,10 @@ public abstract class ServicoMensagemInstantanea {
 	public abstract void enviarMensagem();
 	public abstract void receberMensagem();
 
+	protected void validarConectadoInternet() {
+		System.out.println("Validando se está conectado a internet");
+	}
+	protected void salvarHistoricoMensagem() {
+		System.out.println("Salvando o histórico da mensagem");
+	}
 }
