3f1109aafe9d9cb05ce4fdfe43c7bd5c44561757
Walber Costa <walberoc@gmail.com>
2024-05-25 21:37:33 -0300

    commit inicial pilares poo

---
+++ 19-PILARES-POO/PilaresDaPOO/src/ex1/Carro.java
@@ -0,0 +1,10 @@
+package ex1;
+
+public class Carro {
+
+    public void ligar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'ligar'");
+    }
+
+}
