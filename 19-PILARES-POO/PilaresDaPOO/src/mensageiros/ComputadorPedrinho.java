8de45debde8c6b85a196fcfcb997a24f77bff61d
Walber Costa <walberoc@gmail.com>
2024-05-26 07:14:56 -0300

    forçando um contrato via Abstração

--- 19-PILARES-POO/PilaresDaPOO/src/mensageiros/ComputadorPedrinho.java
+++ 19-PILARES-POO/PilaresDaPOO/src/mensageiros/ComputadorPedrinho.java
@@ -2,16 +2,19 @@ package mensageiros;
 
 public class ComputadorPedrinho {
     public static void main(String[] args) {
+        System.out.println("- MSN -");
         MSNMessenger msn = new MSNMessenger();
         // implementado o encapsulamento o usuário não fica responsável por todos os passos
         msn.enviarMensagem();
         msn.receberMensagem();
 
+        System.out.println("- Facebook -");
         FacebookMessenger fcm = new FacebookMessenger();
         // implementado o encapsulamento o usuário não fica responsável por todos os passos
         fcm.enviarMensagem();
         fcm.receberMensagem();
 
+        System.out.println("- Telegram -");
         Telegram tlg = new Telegram();
         // implementado o encapsulamento o usuário não fica responsável por todos os passos
         tlg.enviarMensagem();
--- 19-PILARES-POO/PilaresDaPOO/src/mensageiros/ServicoMensagemInstantanea.java
+++ 19-PILARES-POO/PilaresDaPOO/src/mensageiros/ServicoMensagemInstantanea.java
@@ -1,20 +1,8 @@
 package mensageiros;
+// agora vou me abstrair e assim não entrar em detalhes de como vocês
+// irão implementar serviços de mensagem. Vou garantir que façam de alguma forma
+public abstract class ServicoMensagemInstantanea {
+	public abstract void enviarMensagem();
+	public abstract void receberMensagem();
 
-public class ServicoMensagemInstantanea {
-	public void enviarMensagem() {
-        // primeiro verifica se está conectado na internet
-        validarConectadoInternet();
-		System.out.println("Enviando mensagem");
-        // depois de enviada, salva no histórico
-        salvarHistoricoMensagem();
-	}
-	public void receberMensagem() {
-		System.out.println("Recebendo mensagem");
-	}
-	private void validarConectadoInternet() {
-		System.out.println("Validando se está conectado a internet");
-	}
-	private void salvarHistoricoMensagem() {
-		System.out.println("Salvando o histórico da mensagem");
-	}
 }
