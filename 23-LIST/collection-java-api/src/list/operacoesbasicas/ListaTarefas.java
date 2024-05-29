08ee5f565899df2ed7bbe3f517c727e7a40a641a
Walber Costa <walberoc@gmail.com>
2024-05-29 19:18:32 -0300

    métodos listar e adicionar ok

--- 23-LIST/collection-java-api/src/list/operacoesbasicas/ListaTarefas.java
+++ 23-LIST/collection-java-api/src/list/operacoesbasicas/ListaTarefas.java
@@ -34,6 +34,18 @@ public class ListaTarefas {
     }
 
     public static void main(String[] args) {
+        ListaTarefas listaTarefas = new ListaTarefas();
+        System.out.println("O número total de elementos na lista é:  "+
+                    listaTarefas.obterNumeroTotalTarefas());
         
+        listaTarefas.adicionarTarefa("Tarefa 1");
+        System.out.println("O número total de elementos na lista é:  "+
+                    listaTarefas.obterNumeroTotalTarefas());
+
+        listaTarefas.adicionarTarefa("Tarefa 2");
+        System.out.println("O número total de elementos na lista é:  "+
+                    listaTarefas.obterNumeroTotalTarefas());
+                                       
+                            
     }
 }
