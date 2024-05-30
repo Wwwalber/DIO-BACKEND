49994aeec0d2f60a18623328476274307ec0f941
Walber Costa <walberoc@gmail.com>
2024-05-30 16:10:47 -0300

    implementação inicial dos métodos

---
+++ 23-LIST/collection-java-api/src/list/carrinhocompras/CarrinhoDeCompras.java
@@ -0,0 +1,39 @@
+package list.carrinhocompras;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+    private List<Item> itensDaLista;
+
+    public CarrinhoDeCompras() {
+        this.itensDaLista = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade){
+        itensDaLista.add(new Item(nome, preco, quantidade));
+    }
+
+    public void removerItem(String nomeDoItem){
+        List<Item> itensParaRemover = new ArrayList<>();
+        for (Item item : itensParaRemover) {
+            if (item.getNome().equalsIgnoreCase(nomeDoItem)) {
+                // preenche com todos os que serão removidos
+                itensParaRemover.add(item);
+            }
+        } // após preencher todos os itens
+        itensDaLista.removeAll(itensParaRemover);
+    }
+
+    public double calcularValorTotal(){
+        double valorCarrinho = 0;
+        for (Item item : itensDaLista) {
+            valorCarrinho = valorCarrinho + item.getPreco();
+        }
+        return itensDaLista.size() * valorCarrinho;
+    }
+
+    public void exibirItens(){
+        System.out.println(itensDaLista);
+    }
+}
