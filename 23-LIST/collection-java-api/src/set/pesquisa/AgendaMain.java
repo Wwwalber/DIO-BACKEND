3e3797a784cbb537ff3e673814c16ec642522d20
Walber Costa <walberoc@gmail.com>
2024-06-01 20:25:07 -0300

    implementação da Agenda com métodos ok

---
+++ 23-LIST/collection-java-api/src/set/pesquisa/AgendaMain.java
@@ -0,0 +1,37 @@
+package set.pesquisa;
+
+public class AgendaMain {
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.exibirContatos();
+
+            // Adicionando contatos à agenda
+        agendaContatos.adicionarContato("João", 123456789);
+        agendaContatos.adicionarContato("João", 987654321);// nesse caso não vai adicionar com o mesmo nome
+        agendaContatos.adicionarContato("Maria", 987654321);
+        agendaContatos.adicionarContato("Maria Fernandes", 55555555);
+        agendaContatos.adicionarContato("Ana", 88889999);
+        agendaContatos.adicionarContato("Fernando", 77778888);
+        agendaContatos.adicionarContato("Carolina", 55555555);
+
+            // Exibindo os contatos na agenda
+        agendaContatos.exibirContatos();
+            // Pesquisando contatos pelo nome
+        String nomeParaPesquisar = "Maria";
+        System.out.println("Pesquisar pelo nome '"+nomeParaPesquisar+"'");
+
+        // Atualizando o número de um contato
+        String contatoParaAtualizar ="Carolina";
+        int numeroParaAtualizar = 44443333;
+        System.out.println("Atualizar o contato '"+contatoParaAtualizar+"' com o número '"+numeroParaAtualizar+"'");
+        
+        Contato contatoAtualizado = agendaContatos.atualizarNumeroContato(contatoParaAtualizar,numeroParaAtualizar);
+
+        System.out.println("Contato atualizado: " + contatoAtualizado);
+
+        // Exibindo os contatos atualizados na agenda
+        System.out.println("Contatos na agenda após atualização:");
+        agendaContatos.exibirContatos();    
+    }
+}
