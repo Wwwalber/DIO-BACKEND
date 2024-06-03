ba83f0fcdc307275e89204c297b7838c83025324
Walber Costa <walberoc@gmail.com>
2024-06-03 18:48:03 -0300

    segunda versão de obterProximoEvento

--- 23-LIST/collection-java-api/src/map/ordenacao/AgendaEventos.java
+++ 23-LIST/collection-java-api/src/map/ordenacao/AgendaEventos.java
@@ -26,15 +26,15 @@ public class AgendaEventos {
 
     public void obterProximoEvento(){
         // tem que ser de hoje em diante!
-
             // Set<LocalDate> dataSet =  eventosMap.keySet();// retorna Set com a cahves
            // Collection<Evento> valores = eventosMap.values();// retorna uma Coleção com todos os valores
             // estas variáveis acima não se conhecem, não tem relações
 
         // prciso de início saber a data atual -> LocalDate.now()
         LocalDate dataAtual = LocalDate.now();
+    
         // abaixo tenho os eventos ordenados
-        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);//ordenou com TreeMap
         for (Map.Entry<LocalDate, Evento> entry : eventosMap.entrySet()) { 
           // pode usar 'var' vs Map.Entry      // o método 'entrySet' consigo retorna um Set com a ligação chave valo (K,V)
             if (entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)) {
@@ -43,5 +43,20 @@ public class AgendaEventos {
                 break;
             }    
         }
+    }
+    public void obterProximoEventoV2(){
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        Map<LocalDate,Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        for (Map.Entry<LocalDate,Evento> entry : eventosMap.entrySet()) {
+            if (entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)) {
+                proximaData = entry.getKey();
+                proximoEvento = entry.getValue();
+                System.out.println("O próximo evento: "+ proximoEvento+
+                " acontecerá na data "+proximaData);
+                break;
+            }
+        }        
     }
 }
