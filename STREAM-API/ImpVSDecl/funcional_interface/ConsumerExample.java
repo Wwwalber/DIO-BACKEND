3c92065868cfa5c9bca5f948cda14eab80c5bd68
Walber <walberoc@gmail.com>
2024-06-06 19:11:07 -0300

    update ignore retirand launch.json

--- STREAM-API/ImpVSDecl/funcional_interface/ConsumerExample.java
+++ STREAM-API/ImpVSDecl/funcional_interface/ConsumerExample.java
@@ -10,11 +10,23 @@ public class ConsumerExample {
         // Usar o Consumer com expressão lambda para imprimir múmeros pares
         Consumer<Integer> imprimirNumeroPar = numero -> {
             if(numero % 2 == 0){ // verifica se é par
-                System.out.println(numero);
+                System.out.print(numero+" ");
             }
         };
         // Usar o consumer para imprimir números pares
         numeros.stream().forEach(imprimirNumeroPar); 
         // transformando a lista 'numeros' em um stream
+        System.out.println("\n------------");
+        // segundo uso, mais prático
+        numeros.stream().forEach(new Consumer<Integer>(){
+
+            @Override
+            public void accept(Integer num) {
+                if (num % 2 == 0) {
+                    System.out.print(num+" ");
+                }
+            }
+            
+        });
     }
 }
