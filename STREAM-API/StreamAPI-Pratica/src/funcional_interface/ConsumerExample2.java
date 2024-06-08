2488e2c8aca13b8662ebfa286c9e3fe02be2dd87
Walber <walberoc@gmail.com>
2024-06-08 14:12:19 -0300

    uso do método stream ok 2º método

--- STREAM-API/StreamAPI-Pratica/src/funcional_interface/ConsumerExample2.java
+++ STREAM-API/StreamAPI-Pratica/src/funcional_interface/ConsumerExample2.java
@@ -2,21 +2,25 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
-import funcional_interface.Pessoa;
 
 public class ConsumerExample2 {
     public static void main(String[] args) {
-        List<String> palavras = Arrays.asList("banana", "gatos", "abacaxi", "cachorro", "computador", "floresta",
+        List<String> palavras = Arrays.asList("manga", "gatos", "abacaxi", "cachorro", "computador", "floresta",
                 "amarelo", "piano", "ventilador", "bicicleta", "chocolate", "oceano", "televisão", "espelho", "morango",
                 "avião", "livro", "futebol", "lápis", "janela", "girafa", "viagem", "relógio", "sorvete", "papagaio",
                 "montanha", "telefone", "piscina", "dinossauro", "sanduíche", "pôr do sol", "escola", "coelho",
                 "violão", "fogo", "hamburguer", "estrela", "música", "pintura", "praia", "bola", "cama", "arco-íris",
-                "borboleta", "chave", "sapato", "lua");
+                "borboleta", "chave", "cadarço", "gelo");
         Consumer<String> imprimeLista = palavra -> {
             System.out.println(palavra);
         };
 
-        palavras.stream().forEach(imprimeLista);
+        palavras.stream().forEach(new Consumer<String>() {
+            @Override
+            public void accept(String pal) {
+                System.out.println(pal);
+            }    
+        });
 
     }
 }
