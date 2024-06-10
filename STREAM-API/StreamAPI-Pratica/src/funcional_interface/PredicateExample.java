2b809a0760972595240b4651ac07a542b0b29d73
Walber <walberoc@gmail.com>
2024-06-10 11:36:37 -0300

    opção informando o predicate dentro do filter

--- STREAM-API/StreamAPI-Pratica/src/funcional_interface/PredicateExample.java
+++ STREAM-API/StreamAPI-Pratica/src/funcional_interface/PredicateExample.java
@@ -6,11 +6,19 @@ public class PredicateExample {
     public static void main(String[] args) {
         List<String> palavras = Arrays.asList("Java", "Kiotlin", "JavaScript", "C", "Python", "C#","Go");
 
-        // cria o Predicate para verificar se e palavra tem mais de 5 caracteres
-        Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() > 5;
+            // cria o Predicate para verificar se e palavra tem mais de 5 caracteres
+            Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() > 5;
+            /* usar o Stream para filtrar as palavras com mais de 5 caracteres e imprime cada
+            palavra que passou no filtro  */
+            palavras.stream().filter(maisDeCincoCaracteres).forEach(System.out::println);
 
-        /* usar o Stream para filtrar as palavras com mais de 5 caracteres e imprime cada
-         palavra que passou no filtro  */
-        palavras.stream().filter(maisDeCincoCaracteres).forEach(System.out::println);
+            System.out.println("-----------");
+            /* e caso eu não saiba implementar o Predicate */
+            palavras.stream().filter(new Predicate<String>() {
+                @Override
+                public boolean test(String t) {
+                return t.length() > 5;
+                }         
+            }).forEach(System.out::println);
     }
 }
