b45900cca43700ca40a12a1930d14d98288ae23a
Walber <walberoc@gmail.com>
2024-06-10 09:41:35 -0300

    1ª implementação ok

---
+++ STREAM-API/StreamAPI-Pratica/src/funcional_interface/funcional_interface/SupplierExample.java
@@ -0,0 +1,21 @@
+package funcional_interface;
+
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SupplierExample {
+    public static void main(String[] args) {
+        // Usar o Supplier com expressão lambda para fornecer uma saudação personalizada
+        Supplier <String> saudacao = () -> "Olá! Seja bem vindo!";
+
+        // Usar o Supplier para obter uma lista com 5 saudações
+        List<String> listaSaudacoes = Stream.generate(saudacao)
+            .limit(5)
+            .collect(Collectors.toList());
+
+        // imprime as saudações geradas
+        listaSaudacoes.forEach(lis -> System.out.println(lis));
+    }
+}
