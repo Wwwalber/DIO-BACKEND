bcf74616240a0cc7ea04da24f61354d2554a17cb
Walber <walberoc@gmail.com>
2024-06-10 14:48:16 -0300

    aprimorando com uso de 'Integer::sum'

--- STREAM-API/StreamAPI-Pratica/src/funcional_interface/BinaryOpetarorExample.java
+++ STREAM-API/StreamAPI-Pratica/src/funcional_interface/BinaryOpetarorExample.java
@@ -7,7 +7,7 @@ public class BinaryOpetarorExample {
                 // cria lista de números inteiros
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
             /* usa o BinaryOperator com função lambda para somar dois números inteiros */
-            BinaryOperator<Integer> somar = (num1, num2) -> num1 + num2 ;
+            BinaryOperator<Integer> somar = Integer::sum ;
                              System.out.println("---- 1ª versão ------");
 
             // usa o BinaryOperator para somar todos os números
@@ -24,7 +24,11 @@ public class BinaryOpetarorExample {
                 }
             });
             System.out.println("A soma dos números é: "+resultado2);
-
+            
+                            System.out.println("---- 3ª versão ------");
+            // implementando com lambda o BinaryOperator
+            int resultado3 = numeros.stream().reduce(0, Integer::sum);
+            System.out.println("A soma dos números é: "+resultado3);
 
     }
 
