b5a68b0431eadd03eb3991952f7d3806ee76f13b
Walber <walberoc@gmail.com>
2024-06-10 16:34:23 -0300

    se a questão é imprimir numa 'lista'

--- STREAM-API/StreamAPI-Pratica/src/desafios/Desafio1.java
+++ STREAM-API/StreamAPI-Pratica/src/desafios/Desafio1.java
@@ -1,12 +1,20 @@
 package desafios;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Desafio1 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 11,4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
-        // mostrar a lista na órdem alfabética
+        // mostrar os números na órdem alfabética
         numeros.stream().sorted().forEach(System.out::println);
+
+                System.out.println("Mostre a lista na ordem númerica: ");
+        List<Integer> numerosOrdemNatural = numeros.stream()
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toList());
+        System.out.println(numerosOrdemNatural);
     }
 }
